7fef0fe5f38b9dd12e170bab581a4274d1c15080
syzm <szymone25@gmail.com>
2024-12-26 10:20:53 +0100

    feat: new pred

--- tvenv/bin/activate.fish
+++ tvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/yosyz/Desktop/tuzuModel/tvenv
+set -gx VIRTUAL_ENV /home/yosyzm/Desktop/tuzuModel/tvenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
