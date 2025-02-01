f2fac47ebc1f39b141d7b59b3042b1d55abdea1d
syzm <szymone25@gmail.com>
2025-02-01 04:24:19 +0100

    feat: reactor for easier start

--- execute.sh
+++
@@ -1,9 +0,0 @@
-python3 '1. repo_cleanup.py' && \
-python3 '2. entry_processing.py' && \
-python3 '3. gk_training.py' && \
-python3 '4. def_training.py' && \
-python3 '5. mid_training.py' && \
-python3 '6. fwd_training.py' && \
-python3 '7. get_prediction_data.py' && \
-python3 '8_predictions.py' && \
-python3 saveToDatabase.py
